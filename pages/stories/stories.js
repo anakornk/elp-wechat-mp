84db8709a97209ee32201e85febddd8ba6d3d198
Allison Ma <c.allison.ma@gmail.com>
2017-08-17 11:50:11 +0800

    wip

---
+++ pages/stories/stories.js
@@ -0,0 +1,53 @@
+Page({
+  data: {
+    winWidth: 0,
+    winHeight: 0,
+    currentTab: 0,
+    textdata: "put value",
+  },
+  onLoad: function () {
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          winWidth: res.windowWidth,
+          winHeight: res.windowHeight
+        });
+      }
+    });
+    wx.request({
+      url: 'http://localhost:3000/stories.json',
+      method: 'GET',
+      header: { 'content-type': 'application/json' },
+      success: function (res) {
+        that.setData({textdata:res.data});
+      },
+      fail: function (res) {
+      },
+    });
+  },
+  bindChange: function (e) {
+    var that = this;
+    that.setData({ currentTab: e.detail.current });
+  },
+  swichNav: function (e) {
+    var that = this;
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+      that.setData({
+        currentTab: e.target.dataset.current
+      })
+    }
+  },
+  tz: function (e) {
+    var arrayIndex = e.target.id;
+    var story_id = this.data.textdata[arrayIndex].id;
+    var root_page_id = this.data.textdata[arrayIndex].root_page_id;
+    var url = '../story/story?story_id=' + story_id + '&page_id=' + root_page_id
+
+    wx.navigateTo({
+      url: url
+    })
+  }
+})
---
+++ pages/story/story.js
@@ -0,0 +1,53 @@
+Page({
+  data: {
+    current_story: '',
+    page_id: '0',
+    textdata: "put value",
+    isLastPage: false
+  },
+  onLoad: function (e) {
+    var story_id = e.story_id;
+    var page_id = e.page_id;
+    this.setData({
+      current_story: story_id
+    })
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          winWidth: res.windowWidth,
+          winHeight: res.windowHeight
+        });
+      }
+    });
+    var url = 'http://localhost:3000/stories/' + '2' + '/pages/' + page_id;
+    wx.request({
+      url: url,
+      method: 'GET',
+      header: { 'content-type': 'application/json' },
+      success: function (res) {
+        console.log(res.data);
+        that.setData({ textdata: res.data });
+        that.setData({ isLastPage: that.checkIfLastPage()})
+      },
+      fail: function (res) {
+      },
+    });
+  },
+  A: function (e) {
+    var nextPageId = e.currentTarget.dataset.nextpageid  //this.data.textdata.links[0].nextPageId
+    var url = '../story/story?story_id=' + this.data.current_story + '&page_id=' + nextPageId
+    // var url = 'http://localhost:3000' + this.data.textdata.links[0].path
+    wx.redirectTo({
+      url: url
+    })
+  },
+  
+  checkIfLastPage(){
+    var length = this.data.textdata.links.filter(function (link) {
+      return link.nextPageId != null;
+    }).length
+
+    return length == 0;
+  }
+})
